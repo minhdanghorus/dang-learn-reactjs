32f21cb1b8d59e5472c455c1474b4bfb3cbd5cfd
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-04-01 22:44:00 +0700

    b9: component là gì?

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+
+const Feature = () => {
+  return (
+    <div className="feature">
+      <img src="" alt="" className="feature-image" />
+      <h3 className="feature-title">Title</h3>
+      <p className="feature-desc">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quia
+        dolorem accusamus, officia explicabo illo quas cum! Vero iure aspernatur
+        nisi corrupti, molestias doloribus, ullam quibusdam minima ipsa fugiat
+        distinctio!
+      </p>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <div>
-      
+      <Feature></Feature>
+      <Feature></Feature>
+      <Feature></Feature>
     </div>
   );
 };
