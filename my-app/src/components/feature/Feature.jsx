c23812c29b6a8d658d97ac3a6e3990e188341416
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-04-07 15:01:42 +0700

    b16: refactor code

---
+++ my-app/src/components/feature/Feature.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Feature = () => {
+    return (
+      <div className="feature">
+        <img src="" alt="" className="feature-image" />
+        <h3 className="feature-title">Title</h3>
+        <p className="feature-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quia
+          dolorem accusamus, officia explicabo illo quas cum! Vero iure aspernatur
+          nisi corrupti, molestias doloribus, ullam quibusdam minima ipsa fugiat
+          distinctio!
+        </p>
+      </div>
+    );
+  };
+
+
+export default Feature;
