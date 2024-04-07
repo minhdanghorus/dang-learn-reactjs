92cfb6691689937a2238e7d15d5ed60cfe6ecd2d
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-04-07 20:33:52 +0700

    b27: stale state là gì

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Toggle from "./components/toggle/Toggle";
+import Counter from "./components/counter/Counter";
 
 const App = () => {
   return (
     <div>
       <Toggle></Toggle>
+      <Counter></Counter>
     </div>
   );
 };
