662829d7e75ac3e94fab1d03d11402f784f6facf
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-04-07 13:56:57 +0700

    b12: Tìm hiểu 2 cách style cơ bản trong react

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 
 const Feature = () => {
   return (
@@ -15,12 +16,35 @@ const Feature = () => {
   );
 };
 
+function YoutubeItem(props) {
+  return (
+    <div className="youtube-item">
+      <div className="youtube-image">
+        <img src={props.image} alt="" />
+      </div>
+      <div className="youtube-footer">
+        <img src={props.avatar} alt="" className="youtube-avatar" />
+        <div className="youtube-info">
+          <h3 className="youtube-title">{props.title || 'This is example title'}</h3>
+          <h4 className="youtube-author">{props.author || 'Default author'}</h4>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 const App = () => {
   return (
-    <div>
+    <div className="youtube-list">
+      {/* <Feature></Feature>
       <Feature></Feature>
-      <Feature></Feature>
-      <Feature></Feature>
+      <Feature></Feature> */}
+      <YoutubeItem
+        image="https://images.unsplash.com/photo-1707343848552-893e05dba6ac?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        avatar="https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        title="Nghề lập trình viên đang quá cạnh tranh"
+        author="Nguyễn Trần Minh Đăng"
+      />
     </div>
   );
 };
