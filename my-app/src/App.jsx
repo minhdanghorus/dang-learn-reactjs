93eee7d4992023687ada92bbbcfa59ee4e7c2ba2
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-04-07 14:55:22 +0700

    b16: Refactor code

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import YoutubeList from "./components/youtube/YoutubeList";
 
 const Feature = () => {
   return (
@@ -16,37 +17,13 @@ const Feature = () => {
   );
 };
 
-function YoutubeItem(props) {
-  return (
-    <div className="youtube-item">
-      <div className="youtube-image">
-        <img src={props.image} alt="" />
-      </div>
-      <div className="youtube-footer">
-        <img src={props.avatar} alt="" className="youtube-avatar" />
-        <div className="youtube-info">
-          <h3 className="youtube-title">{props.title || 'This is example title'}</h3>
-          <h4 className="youtube-author">{props.author || 'Default author'}</h4>
-        </div>
-      </div>
-    </div>
-  );
-}
 
 const App = () => {
   return (
-    <div className="youtube-list">
-      {/* <Feature></Feature>
-      <Feature></Feature>
-      <Feature></Feature> */}
-      <YoutubeItem
-        image="https://images.unsplash.com/photo-1707343848552-893e05dba6ac?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        avatar="https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        title="Nghề lập trình viên đang quá cạnh tranh"
-        author="Nguyễn Trần Minh Đăng"
-      />
+    <div>
+      <YoutubeList />
     </div>
-  );
+  )
 };
 
 export default App;
---
+++ my-app/src/data.js
@@ -0,0 +1,29 @@
+export const YoutubeData = [
+  {
+    id: 1,
+    title: "Nghề lập trình viên đang quá cạnh tranh",
+    author: "Nguyễn Trần Minh Đăng",
+    image:
+      "https://images.unsplash.com/photo-1707343848552-893e05dba6ac?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    avatar:
+      "https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    title: "Nghề lập trình viên đang quá cạnh tranh",
+    author: "Nguyễn Trần Minh Đăng",
+    image:
+      "https://images.unsplash.com/photo-1707343848552-893e05dba6ac?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    avatar:
+      "https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    title: "Nghề lập trình viên đang quá cạnh tranh",
+    author: "Nguyễn Trần Minh Đăng",
+    image:
+      "https://images.unsplash.com/photo-1707343848552-893e05dba6ac?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    avatar:
+      "https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
