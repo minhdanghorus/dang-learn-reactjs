e1a09405c7c9ecfb47a4575d2f6727f6c798adc0
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-04-21 17:18:41 +0700

    b49: Tìm hiểu một cách sử dụng styled-components khác

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Card from "./components/card/Card";
+import Card2 from "./components/card/Card2";
 import CardList from "./components/card/CardList";
 import { GlobalStyles } from "./GlobalStyles";
 
@@ -10,7 +11,7 @@ const App = () => {
       <GlobalStyles></GlobalStyles>
       <CardList>
         <Card></Card>
-        <Card></Card>
+        <Card2 secondary></Card2>
         <Card></Card>
         <Card></Card>
         <Card></Card>
