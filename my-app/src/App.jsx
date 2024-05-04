065a462a9d0497176c0d667f6fb4da1778df238a
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-05-04 20:19:44 +0700

    b52, 52, 53, 54: Sử dụng TailwinCSS

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,30 +1,36 @@
 import React from "react";
 import "./App.css";
-import Card from "./components/card/Card";
-import Card2 from "./components/card/Card2";
+// import Card from "./components/card/Card";
+// import Card2 from "./components/card/Card2";
+import CardTailwind from "./components/card/CardTailwind";
 import CardList from "./components/card/CardList";
-import { GlobalStyles } from "./GlobalStyles";
-import { ThemeProvider } from "styled-components";
-const theme = {
-  colors: {
-    blue: "#ff6bcb",
-  },
-  orange: "#ffa400",
-};
+// import { GlobalStyles } from "./GlobalStyles";
+// import { ThemeProvider } from "styled-components";
+// const theme = {
+//   colors: {
+//     blue: "#ff6bcb",
+//   },
+//   orange: "#ffa400",
+// };
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles></GlobalStyles>
+    // <ThemeProvider theme={theme}>
+    //   <GlobalStyles></GlobalStyles>
+    //   <CardList>
+    //     <Card></Card>
+    //     <Card2 secondary></Card2>
+    //     <Card></Card>
+    //     <Card></Card>
+    //     <Card></Card>
+    //     <Card></Card>
+    //   </CardList>
+    // </ThemeProvider>
+    <div>
       <CardList>
-        <Card></Card>
-        <Card2 secondary></Card2>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+        <CardTailwind></CardTailwind>
       </CardList>
-    </ThemeProvider>
+    </div>
   );
 };
 
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "primary-gradient": `linear-gradient(
+          86.88deg,
+          #7d6aff 1.38%,
+          #ffb86c 64.35%,
+          #fc2872 119.91%
+        )`,
+        "secondary-gradient": `linear-gradient(86.88deg, #20e3b2, #2cccff)`,
+      },
+    },
   },
   plugins: [],
 }
