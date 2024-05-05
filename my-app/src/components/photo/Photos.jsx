c7b6d8e8ec06f1ddfb2c326a3b8120418e48e794
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-05-05 23:38:14 +0700

    b63, 64, 65: Thực hành với API Picsum photos sử dụng useEffect

--- my-app/src/components/photo/Photos.jsx
+++ my-app/src/components/photo/Photos.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const getRandomPhotos = () => {
+const getRandomPhotos = (page) => {
   return axios
-    .get("https://picsum.photos/v2/list")
+    .get(`https://picsum.photos/v2/list?page=${page}&limit=8`)
     .then((response) => {
       console.log(response);
       return response.data;
@@ -13,16 +13,44 @@ const getRandomPhotos = () => {
     });
 };
 
-// https://picsum.photos/v2/list
 const Photos = () => {
   const [randomPhotos, setRandomPhotos] = useState([]);
+  const [nextPage, setNextPage] = useState(1);
+
+  const handleLoadMorePhotos = () => {
+    getRandomPhotos(nextPage).then((images) => {
+      setRandomPhotos([...randomPhotos, ...images]);
+      setNextPage(nextPage + 1);
+    });
+  };
+
   useEffect(() => {
-    getRandomPhotos().then((images) => {
-        setRandomPhotos(images);
-    })
+    handleLoadMorePhotos();
   }, []);
-  console.log("outside");
-  return <div>{JSON.stringify(randomPhotos)}</div>;
+  return (
+    <div>
+      <div className=" grid grid-cols-4 gap-5 p-5">
+        {randomPhotos.length > 0 &&
+          randomPhotos.map((item, index) => (
+            <div key={item.id} className=" p-3 bg-white shadow-md rounded-lg">
+              <img
+                src={item.download_url}
+                alt={item.author}
+                className="w-full h-48 object-cover rounded-lg"
+              />
+            </div>
+          ))}
+      </div>
+      <div className=" text-center">
+        <button
+          onClick={handleLoadMorePhotos}
+          className=" inline-block px-8 py-4 bg-purple-600 text-white"
+        >
+          Load more
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Photos;
