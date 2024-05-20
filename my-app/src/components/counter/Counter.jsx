03b86f4631c807756765335af959073505b06e7c
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-05-20 23:01:50 +0700

    b68: Tìm hiểu dependency trong useEffect phần 3

--- my-app/src/components/counter/Counter.jsx
+++ my-app/src/components/counter/Counter.jsx
@@ -2,20 +2,25 @@ import React, { useEffect, useState } from "react";
 
 const Counter = () => {
   // stale state
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   // const handleIncrement = () => {
   //   setTimeout(function delay() {
   //     setCount((count) => count + 1);
   //   }, 3000);
   // };
   // return <div onClick={handleIncrement}>Increment {count}</div>;
+  const [infor , setInfor] = useState({
+    firstName: "Tran Minh Dang",
+    lastName: "Nguyen",
+  })
   useEffect(() => {
-    console.log("Counter component mounted");
-  }, [count]);
+    console.log("input form");
+  }, [infor.lastName]);
 
   return <div className=" p-5 flex gap-x-4 items-center">
-    <span className=" text-2xl font-bold">{count}</span>
-    <button onClick={() => setCount(count + 1)} className=" px-4 py-2 bg-blue-500 text-white rounded-lg">Increment</button>
+    {/* <span className=" text-2xl font-bold">{count}</span> */}
+    <input type="text" value={infor.firstName} onChange={(e) => setInfor({...infor, firstName: e.target.value})} className=" px-4 py-2 border border-gray-300 rounded-lg" />
+    {/* <button onClick={() => setCount(count + 1)} className=" px-4 py-2 bg-blue-500 text-white rounded-lg">Increment</button> */}
   </div>;
 };
 
