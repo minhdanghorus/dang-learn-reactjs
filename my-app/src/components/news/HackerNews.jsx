0be586a3bcfa75aee3ddf96d40d62aa0e16e1cec
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-08-01 14:21:41 +0700

    b76: Tối ưu giao diện HackerNews API

--- my-app/src/components/news/HackerNews.jsx
+++ my-app/src/components/news/HackerNews.jsx
@@ -6,50 +6,53 @@ import lodash from "lodash";
 
 const HackerNews = () => {
   const [hits, setHits] = React.useState([]);
-  const [query, setQuery] = React.useState("react");
+  const [query, setQuery] = React.useState("");
   const [loading, setLoading] = React.useState(true);
   const [errorMessage, setErrorMessage] = React.useState("");
   const handleFetchData = useRef({});
   handleFetchData.current = async () => {
     setLoading(true);
     try {
-
-        const response = await axios.get(
-            `https://hn.algolia.com/api/v1/search?query=${query}`
-        );
-        console.log("response: ", response);
-        setHits(response.data?.hits || []);
-        setLoading(false);
-    }
-    catch (error) {
-        console.log("error: ", error);
-        setLoading(false);
-        setErrorMessage(`There was an error fetching data ${error}`);
+      const response = await axios.get(
+        `https://hn.algolia.com/api/v1/search?query=${query}`
+      );
+      console.log("response: ", response);
+      setHits(response.data?.hits || []);
+      setLoading(false);
+    } catch (error) {
+      console.log("error: ", error);
+      setLoading(false);
+      setErrorMessage(`There was an error fetching data ${error}`);
     }
   };
 
   // Add lodash debounce to prevent multiple API calls
   const handleUpdateQuery = lodash.debounce((e) => {
     setQuery(e.target.value);
-  }, 2000);
+  }, 1000);
 
   React.useEffect(() => {
     handleFetchData.current();
   }, [query]);
 
   return (
-    <div>
+    <div className=" bg-white mx-auto mt-5 mb-5 p-5 rounded-lg shadow-md w-2/4">
       <input
         type="text"
-        className="border border-green-500 text-black p-5 mb-5"
+        className="border border-gray-200 p-5 block w-full rounded-md transition-all focus:border-blue-400"
+        placeholder="Typing your keyword..."
         defaultValue={query}
         onChange={handleUpdateQuery}
       />
-      {!loading && errorMessage && <p>{errorMessage}</p>}
-      {loading && <div className="loading w-8 h-8 rounded-full border-blue-500 border-4 border-r-4 border-r-transparent animate-spin"></div>}
-      {!loading &&
-        hits.length > 0 &&
-        hits.map((item, index) => <h3 key={item.title}>{item.title}</h3>)}
+      {!loading && errorMessage && <p className=" text-red-400 my-5">{errorMessage}</p>}
+      {loading && (
+        <div className="loading w-8 h-8 rounded-full border-blue-500 border-4 border-r-4 border-r-transparent animate-spin mx-auto my-10"></div>
+      )}
+      <div className="flex flex-wrap gap-5">
+        {!loading &&
+          hits.length > 0 &&
+          hits.map((item, index) => <h3 key={item.title} className=" p-3 bg-gray-200 mt-5 rounded-md">{item.title}</h3>)}
+      </div>
     </div>
   );
 };
