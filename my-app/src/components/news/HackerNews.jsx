b5c8de3ae0cc996a948fe43e1c09f1914e4805e9
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-08-01 22:09:00 +0700

    b77: Bài tập HackerNews API - Fetching dữ liệu với button

--- my-app/src/components/news/HackerNews.jsx
+++ my-app/src/components/news/HackerNews.jsx
@@ -9,13 +9,14 @@ const HackerNews = () => {
   const [query, setQuery] = React.useState("");
   const [loading, setLoading] = React.useState(true);
   const [errorMessage, setErrorMessage] = React.useState("");
+  const [url, setUrl] = React.useState(
+    `https://hn.algolia.com/api/v1/search?query=${query}`
+  );
   const handleFetchData = useRef({});
   handleFetchData.current = async () => {
     setLoading(true);
     try {
-      const response = await axios.get(
-        `https://hn.algolia.com/api/v1/search?query=${query}`
-      );
+      const response = await axios.get(url);
       console.log("response: ", response);
       setHits(response.data?.hits || []);
       setLoading(false);
@@ -27,31 +28,50 @@ const HackerNews = () => {
   };
 
   // Add lodash debounce to prevent multiple API calls
-  const handleUpdateQuery = lodash.debounce((e) => {
-    setQuery(e.target.value);
-  }, 1000);
+  // const handleUpdateQuery = lodash.debounce((e) => {
+  //   setQuery(e.target.value);
+  // }, 1000);
 
   React.useEffect(() => {
     handleFetchData.current();
-  }, [query]);
+  }, [url]);
 
   return (
     <div className=" bg-white mx-auto mt-5 mb-5 p-5 rounded-lg shadow-md w-2/4">
-      <input
-        type="text"
-        className="border border-gray-200 p-5 block w-full rounded-md transition-all focus:border-blue-400"
-        placeholder="Typing your keyword..."
-        defaultValue={query}
-        onChange={handleUpdateQuery}
-      />
-      {!loading && errorMessage && <p className=" text-red-400 my-5">{errorMessage}</p>}
+      <div className="flex mb-5 gap-x-5">
+        <input
+          type="text"
+          className="border border-gray-200 p-5 block w-full rounded-md transition-all focus:border-blue-400"
+          placeholder="Typing your keyword..."
+          defaultValue={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`);
+          }}
+          className=" bg-blue-500 text-white font-semibold rounded-md flex-shrink-0 px-2"
+        >
+          Fetching
+        </button>
+      </div>
+      {!loading && errorMessage && (
+        <p className=" text-red-400 my-5">{errorMessage}</p>
+      )}
       {loading && (
         <div className="loading w-8 h-8 rounded-full border-blue-500 border-4 border-r-4 border-r-transparent animate-spin mx-auto my-10"></div>
       )}
       <div className="flex flex-wrap gap-5">
         {!loading &&
           hits.length > 0 &&
-          hits.map((item, index) => <h3 key={item.title} className=" p-3 bg-gray-200 mt-5 rounded-md">{item.title}</h3>)}
+          hits.map((item, index) => {
+            if (!item.title || item.title.length <= 0) return null;
+            return (
+              <h3 key={item.title} className=" p-3 bg-gray-200 mt-5 rounded-md">
+                {item.title}
+              </h3>
+            );
+          })}
       </div>
     </div>
   );
