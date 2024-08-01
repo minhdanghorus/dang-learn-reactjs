28f4bb882992e76751759a83d32fc603a8ece82b
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-08-01 14:03:05 +0700

    b73-74: Bài tập HackerNews API phần 3,4

--- my-app/src/components/news/HackerNews.jsx
+++ my-app/src/components/news/HackerNews.jsx
@@ -1,19 +1,37 @@
 import React, { useRef } from "react";
 import axios from "axios";
+import lodash from "lodash";
 
 //https://hn.algolia.com/api/v1/search?query=react
 
 const HackerNews = () => {
   const [hits, setHits] = React.useState([]);
   const [query, setQuery] = React.useState("react");
+  const [loading, setLoading] = React.useState(true);
+  const [errorMessage, setErrorMessage] = React.useState("");
   const handleFetchData = useRef({});
   handleFetchData.current = async () => {
-    const response = await axios.get(
-      `https://hn.algolia.com/api/v1/search?query=${query}`
-    );
-    console.log("response: ", response);
-    setHits(response.data?.hits || []);
+    setLoading(true);
+    try {
+
+        const response = await axios.get(
+            `https://hn.algolia.com/api/v1/search?query=${query}`
+        );
+        console.log("response: ", response);
+        setHits(response.data?.hits || []);
+        setLoading(false);
+    }
+    catch (error) {
+        console.log("error: ", error);
+        setLoading(false);
+        setErrorMessage(`There was an error fetching data ${error}`);
+    }
   };
+
+  // Add lodash debounce to prevent multiple API calls
+  const handleUpdateQuery = lodash.debounce((e) => {
+    setQuery(e.target.value);
+  }, 2000);
 
   React.useEffect(() => {
     handleFetchData.current();
@@ -21,8 +39,16 @@ const HackerNews = () => {
 
   return (
     <div>
-      <input type="text" className="border border-green-500 text-black p-5 mb-5" value={query} onChange={(e) => setQuery(e.target.value)}/>
-      {hits.length > 0 &&
+      <input
+        type="text"
+        className="border border-green-500 text-black p-5 mb-5"
+        defaultValue={query}
+        onChange={handleUpdateQuery}
+      />
+      {!loading && errorMessage && <p>{errorMessage}</p>}
+      {loading && <div className="loading w-8 h-8 rounded-full border-blue-500 border-4 border-r-4 border-r-transparent animate-spin"></div>}
+      {!loading &&
+        hits.length > 0 &&
         hits.map((item, index) => <h3 key={item.title}>{item.title}</h3>)}
     </div>
   );
