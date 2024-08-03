ee6c56dccaec8fe5f56f9edf77b3e6ac8843f09c
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-08-03 16:17:59 +0700

    fix: refactor Stop Watch code in b85

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -10,6 +10,7 @@ import Header from "./components/Header";
 import Timer from "./components/Timer";
 import HackerNews from "./components/news/HackerNews";
 import HackerNewsWithReducer from "./components/news/HackerNewsWithReducer";
+import StopWatch from "./components/StopWatch";
 // import { GlobalStyles } from "./GlobalStyles";
 // import { ThemeProvider } from "styled-components";
 // const theme = {
@@ -20,44 +21,18 @@ import HackerNewsWithReducer from "./components/news/HackerNewsWithReducer";
 // };
 
 const App = () => {
-  const timerRef = React.useRef();
-  const [count, setCount] = React.useState(0);
-
-  const handleStart = () => {
-    if (timerRef.current) return;
-    timerRef.current = setInterval(() => {
-      setCount((prevCount) => prevCount + 1);
-    }, 1000);
-  };
-
-  const handleStop = () => {
-    clearInterval(timerRef.current);
-    timerRef.current = null;
-  };
-  console.log("==render App");
-
-  // Khi chúng ta chuyển trang (coponent re-render) thì useEffect sẽ chạy để clear interval cái timerRer đi, tránh trường hợp memory leak do chạy hoài.
-  useEffect(() => {
-    return () => {
-      clearInterval(timerRef.current);
-    };
-  }, []);
 
   return (
     <div>
-      <h3>Timer: {count}</h3>
-      <button onClick={handleStart}>Start</button>
-      <button onClick={handleStop}>Stop</button>
+      {/* <Photos></Photos> */}
+      {/* <Counter></Counter> */}
+      {/* <Timer></Timer> */}
+      {/* <Header></Header> */}
+      {/* <Photos></Photos> */}
+      {/* <HackerNews></HackerNews> */}
+      {/* <HackerNewsWithReducer></HackerNewsWithReducer> */}
+      <StopWatch></StopWatch>
     </div>
-    // <div>
-    //   {/* <Photos></Photos> */}
-    //   {/* <Counter></Counter> */}
-    //   {/* <Timer></Timer> */}
-    //   {/* <Header></Header> */}
-    //   {/* <Photos></Photos> */}
-    //   {/* <HackerNews></HackerNews> */}
-    //   {/* <HackerNewsWithReducer></HackerNewsWithReducer> */}
-    // </div>
   );
 };
 
