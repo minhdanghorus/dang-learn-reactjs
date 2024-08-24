ccaae941ecfdd0f2162b4943cbb6f01d2dd9199e
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-08-24 20:55:04 +0700

    b124: Validation cơ bản với formik

--- my-app/src/components/form/SignUpForm.jsx
+++ my-app/src/components/form/SignUpForm.jsx
@@ -2,10 +2,31 @@ import React from "react";
 import { useFormik } from "formik";
 
 const SignUpForm = () => {
+  const validate = (values) => {
+    const errors = {};
+    if (!values.firstName) {
+      errors.firstName = "Required";
+    }
+    else if (values.firstName.length > 15) {
+      errors.firstName = "Must be 15 characters or less";
+    }
+
+    if (!values.lastName) {
+      errors.lastName = "Required";
+    }
+    else if (values.lastName.length > 15) {
+      errors.lastName = "Must be 15 characters or less";
+    }
+
+    return errors;
+  }
+
   const formik = useFormik({
     initialValues: {
       firstName: "",
+      lastName: "",
     },
+    validate,
     onSubmit: (values) => {
       console.log(values);
     }
@@ -25,9 +46,21 @@ const SignUpForm = () => {
           value={formik.values.firstName}
           onChange={formik.handleChange}
         />
+        {formik.errors.firstName ? <div className=" text-sm text-red-500 ">{formik.errors.firstName}</div> : null}
+        <label htmlFor="lastName">Lastname</label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          placeholder="Enter your last name"
+          className="p-4 rounded-md border border-gray-100"
+          value={formik.values.lastName}
+          onChange={formik.handleChange}
+        />
+        {formik.errors.lastName ? <div className=" text-sm text-red-500 ">{formik.errors.lastName}</div> : null}
       </div>
       <div>
-        <button className=" w-full p-4 bg-blue-600 text-white font-semibold rounded-lg">Submit</button>
+        <button type="submit" className=" w-full p-4 bg-blue-600 text-white font-semibold rounded-lg">Submit</button>
       </div>
     </form>
   );
