e955b1ad07a4d592fe914958c2d4238d7f1f87bf
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-08-24 20:34:17 +0700

    b123: Cài đặt và sử dụng formik vào form

--- my-app/src/components/form/SignUpForm.jsx
+++ my-app/src/components/form/SignUpForm.jsx
@@ -1,18 +1,35 @@
 import React from "react";
+import { useFormik } from "formik";
 
 const SignUpForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    }
+  });
+  console.log("formik: ", formik);
+  
   return (
-    <div className="p-10 w-full max-w-[500px] mx-auto">
+    <form onSubmit={formik.handleSubmit} className="p-10 w-full max-w-[500px] mx-auto">
       <div className="flex flex-col gap-2 mb-5">
         <label htmlFor="firstName">Firstname</label>
         <input
           type="text"
           id="firstName"
+          name="firstName"
           placeholder="Enter your first name"
           className="p-4 rounded-md border border-gray-100"
+          value={formik.values.firstName}
+          onChange={formik.handleChange}
         />
       </div>
-    </div>
+      <div>
+        <button className=" w-full p-4 bg-blue-600 text-white font-semibold rounded-lg">Submit</button>
+      </div>
+    </form>
   );
 };
 
