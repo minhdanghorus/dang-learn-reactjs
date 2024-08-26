0824d7c026019636cf0dfcbca391d459e6d85ef4
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-08-26 22:07:17 +0700

    b126: Validation với thư viện Yup

--- my-app/src/components/form/SignUpForm.jsx
+++ my-app/src/components/form/SignUpForm.jsx
@@ -3,39 +3,39 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 
 const SignUpForm = () => {
-  const validate = (values) => {
-    const errors = {};
-    if (!values.firstName) {
-      errors.firstName = "Required";
-    }
-    else if (values.firstName.length > 15) {
-      errors.firstName = "Must be 15 characters or less";
-    }
+  // const validate = (values) => {
+  //   const errors = {};
+  //   if (!values.firstName) {
+  //     errors.firstName = "Required";
+  //   }
+  //   else if (values.firstName.length > 15) {
+  //     errors.firstName = "Must be 15 characters or less";
+  //   }
 
-    if (!values.lastName) {
-      errors.lastName = "Required";
-    }
-    else if (values.lastName.length > 15) {
-      errors.lastName = "Must be 15 characters or less";
-    }
+  //   if (!values.lastName) {
+  //     errors.lastName = "Required";
+  //   }
+  //   else if (values.lastName.length > 15) {
+  //     errors.lastName = "Must be 15 characters or less";
+  //   }
 
-    return errors;
-  }
+  //   return errors;
+  // }
 
   const formik = useFormik({
     initialValues: {
       firstName: "",
       lastName: "",
     },
-    validate,
-    // validationSchema: Yup.object({
-    //   firstName: Yup.string()
-    //     .max(15, "Must be 15 characters or less")
-    //     .required("Required"),
-    //   lastName: Yup.string()
-    //     .max(20, "Must be 20 characters or less")
-    //     .required("Required"),
-    // }),
+    // validate,
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      lastName: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .required("Required"),
+    }),
     onSubmit: (values) => {
       console.log(values);
     }
@@ -49,24 +49,18 @@ const SignUpForm = () => {
         <input
           type="text"
           id="firstName"
-          name="firstName"
           placeholder="Enter your first name"
           className="p-4 rounded-md border border-gray-100"
-          value={formik.values.firstName}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
+          {...formik.getFieldProps("firstName")}
         />
         {formik.touched.firstName && formik.errors.firstName ? <div className=" text-sm text-red-500 ">{formik.errors.firstName}</div> : null}
         <label htmlFor="lastName">Lastname</label>
         <input
           type="text"
           id="lastName"
-          name="lastName"
           placeholder="Enter your last name"
           className="p-4 rounded-md border border-gray-100"
-          value={formik.values.lastName}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
+          {...formik.getFieldProps("lastName")}
         />
         {formik.touched.lastName && formik.errors.lastName ? <div className=" text-sm text-red-500 ">{formik.errors.lastName}</div> : null}
       </div>
