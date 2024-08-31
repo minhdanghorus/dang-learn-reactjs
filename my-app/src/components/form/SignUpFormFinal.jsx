0099dd71d1b403a09c122702a91d8f37ab4f72dc
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-08-31 16:56:52 +0700

    b131: Bài tập SignUpForm - Tối ưu components và validtion

--- my-app/src/components/form/SignUpFormFinal.jsx
+++ my-app/src/components/form/SignUpFormFinal.jsx
@@ -11,7 +11,7 @@ const SignUpFormV2 = () => {
         email: "",
         intro: "",
         job: "",
-        tern: false,
+        term: false,
       }}
       validationSchema={Yup.object({
         firstName: Yup.string().required("Required"),
@@ -19,61 +19,28 @@ const SignUpFormV2 = () => {
         email: Yup.string().email().required("Required"),
         intro: Yup.string().required("Required"),
         job: Yup.string().required("Required"),
-        tern: Yup.boolean(),
+        term: Yup.boolean().oneOf(
+          [true],
+          "You must accept the terms and conditions"
+        ),
       })}
       onSubmit={(values) => {
         console.log(values);
       }}
     >
       <Form className="p-10 w-full max-w-[500px] mx-auto">
-        {/* <div className="flex flex-col gap-2 mb-5">
-          <label htmlFor="firstName">Firstname</label>
-          <Field
-            type="text"
-            placeholder="Enter your first name"
-            className="p-4 rounded-md border border-gray-100"
-            name="firstName"
-          ></Field>
-          <div className="text-sm text-red-500">
-            <ErrorMessage name="firstName" />
-          </div>
-        </div> */}
         <MyInput
           label="First name"
           name="firstName"
           placeholder="Enter your first name"
           id="firstName"
         ></MyInput>
-        {/* <div className="flex flex-col gap-2 mb-5">
-          <label htmlFor="lastName">Last name</label>
-          <Field
-            type="text"
-            name="lastName"
-            placeholder="Enter your last name"
-            className="p-4 rounded-md border border-gray-100"
-          ></Field>
-          <div className="text-sm text-red-500">
-            <ErrorMessage name="lastName" />
-          </div>
-        </div> */}
         <MyInput
           label="Last name"
           name="lastName"
           placeholder="Enter your last name"
           id="lastName"
         ></MyInput>
-        {/* <div className="flex flex-col gap-2 mb-5">
-          <label htmlFor="email">Email Address</label>
-          <Field
-            type="email"
-            name="email"
-            placeholder="Enter your Email Address"
-            className="p-4 rounded-md border border-gray-100"
-          ></Field>
-          <div className="text-sm text-red-500">
-            <ErrorMessage name="email" />
-          </div>
-        </div> */}
         <MyInput
           label="Email"
           name="email"
@@ -81,35 +48,18 @@ const SignUpFormV2 = () => {
           id="email"
           placeholder="Enter your Email Address"
         ></MyInput>
-        <div className="flex flex-col gap-2 mb-5">
-          <label htmlFor="intro">Intro yourself</label>
-          <Field
-            type="intro"
-            name="intro"
-            placeholder="Enter your introduce"
-            className="p-4 rounded-md border border-gray-100 h-[150px] resize-none"
-            as="textarea"
-          ></Field>
-          <div className="text-sm text-red-500">
-            <ErrorMessage name="intro" />
-          </div>
-        </div>
-        <div className="flex flex-col gap-2 mb-5">
-          <label htmlFor="job">Select your job</label>
-          <Field
-            type="job"
-            className="p-4 rounded-md border border-gray-100"
-            as="select"
-          >
-            <option value="designer">Designer</option>
-            <option value="developer">Developer</option>
-            <option value="tester">Tester</option>
-          </Field>
-          <div className="text-sm text-red-500">
-            <ErrorMessage name="job" />
-          </div>
-        </div>
-        <div className="flex items-center gap-2 mb-5">
+        <MyTextarea
+          label="Intro yourself"
+          name="intro"
+          placeholder="Enter your introduce"
+          id="intro"
+        ></MyTextarea>
+        <MySelectBox label="Select your job" name="job" id="job">
+          <option value="designer">Designer</option>
+          <option value="developer">Developer</option>
+          <option value="tester">Tester</option>
+        </MySelectBox>
+        {/* <div className="flex items-center gap-2 mb-5">
           <Field
             name="terms"
             type="checkbox"
@@ -119,7 +69,10 @@ const SignUpFormV2 = () => {
           <div className="text-sm text-red-500">
             <ErrorMessage name="terms" />
           </div>
-        </div>
+        </div> */}
+        <MyCheckbox name="term" type="checkbox">
+          <p>I accept the terms and conditions</p>
+        </MyCheckbox>
         <div>
           <button
             type="submit"
@@ -145,6 +98,59 @@ const MyInput = ({ label, ...props }) => {
         {...field}
         {...props}
       />
+      {meta.touched && meta.error ? (
+        <div className="text-sm text-red-500">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+const MyTextarea = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  console.log("field: ", field);
+  return (
+    <div className="flex flex-col gap-2 mb-5">
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <textarea
+        type="text"
+        className="p-4 rounded-md border border-gray-100 h-[150px] resize-none"
+        {...field}
+        {...props}
+      />
+      {meta.touched && meta.error ? (
+        <div className="text-sm text-red-500">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+const MySelectBox = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  console.log("field: ", field);
+  return (
+    <div className="flex flex-col gap-2 mb-5">
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <select
+        type="text"
+        className="p-4 rounded-md border border-gray-100"
+        {...field}
+        {...props}
+      />
+      {meta.touched && meta.error ? (
+        <div className="text-sm text-red-500">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+const MyCheckbox = ({ children, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div className="flex flex-col gap-2 mb-5">
+      <label className="flex items-center gap-2 mb-5">
+        <input type="checkbox" {...field} {...props} />
+        {children}
+      </label>
       {meta.touched && meta.error ? (
         <div className="text-sm text-red-500">{meta.error}</div>
       ) : null}
