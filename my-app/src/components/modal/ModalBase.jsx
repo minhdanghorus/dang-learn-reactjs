06a76c444b862c32a1db5d5c4b1cef67ae942b27
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-10-14 09:34:04 +0700

    b182: Tối ưu code Portal nâng cao phần 3

--- my-app/src/components/modal/ModalBase.jsx
+++ my-app/src/components/modal/ModalBase.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import Portal from "../Portal";
+import { CSSTransition } from "react-transition-group";
 
 const ModalBase = ({ visible, onClose, children }) => {
   if (!visible) return null;
   return (
     <>
-      <Portal visible={visible} onClose={onClose} containerClassName="flex items-center justify-center">
-        {children}
-      </Portal>
+      <CSSTransition in={visible} timeout={250} unmountOnExit className="zoom">
+        {(status) => {
+          return (
+            <Portal
+              visible={status !== "exited"}
+              onClose={onClose}
+              containerClassName="flex items-center justify-center"
+            >
+              {children}
+            </Portal>
+          );
+        }}
+      </CSSTransition>
     </>
   );
 };
