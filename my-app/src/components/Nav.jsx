98b41732c1bec046e263de0b03fdd1c6d850e8fc
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-10-24 00:34:45 +0700

    b203: Tìm hiểu React Router cơ bản phần 2

--- my-app/src/components/Nav.jsx
+++ my-app/src/components/Nav.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
+const ListLink = [
+  {
+    id: 1,
+    to: "/",
+    title: "Home",
+  },
+  {
+    id: 2,
+    to: "/blog",
+    title: "Blog",
+  },
+  {
+    id: 3,
+    to: "/profile",
+    title: "Profile",
+  },
+];
+
 const Nav = () => {
   return (
     <div className="p-5 bg-white shadow-md flex items-center justify-center gap-x-5">
-      <NavLink
+      {ListLink.map((item) => (
+        <NavLink
+          key={item.id}
+          to={item.to}
+          className={({ isActive }) => (isActive ? "text-green-400" : "")}
+        >
+          {item.title}
+        </NavLink>
+      ))}
+      {/* <NavLink
         to={"/"}
         className={({ isActive }) => (isActive ? "text-green-400" : "")}
       >
@@ -21,7 +48,7 @@ const Nav = () => {
         className={({ isActive }) => (isActive ? "text-green-400" : "")}
       >
         Profile
-      </NavLink>
+      </NavLink> */}
     </div>
   );
 };
