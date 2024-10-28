578d6d90e8b9fcc8ffe563806fa3ce3915d2b691
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-10-28 21:44:07 +0700

    b208: Code giao diện movie card

--- react-simple-movie/src/App.jsx
+++ react-simple-movie/src/App.jsx
@@ -3,11 +3,11 @@
 function App() {
   return (
     <>
-      <header className="header flex gap-x-5 items-center justify-center text-white py-10 mb-10">
+      <header className="header flex gap-x-5 items-center justify-center text-white py-10 mb-5">
         <span className="text-primary">Home</span>
         <span>Movies</span>
       </header>
-      <section className="banner h-[400px] page-container">
+      <section className="banner h-[500px] page-container mb-20">
         <div className="w-full h-full rounded-lg relative">
           <div className="overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] rounded-lg"></div>
           <img
@@ -29,6 +29,78 @@ function App() {
               </span>
             </div>
             <button className="py-3 px-6 bg-primary rounded-lg text-white font-medium">
+              Watch now
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="movies-layout page-container pb-20">
+        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
+          Now playing
+        </h2>
+        <div className="movie-list grid grid-cols-4 gap-10">
+          <div className="movie-card rounded-lg p-3 bg-slate-800 text-white">
+            <img
+              src="https://s3.cloud.cmctelecom.vn/tinhte1/2018/05/4308115_preview.medium.jpg"
+              alt=""
+              className="w-full h-[250px] rounded-lg object-cover mb-5"
+            />
+            <h3 className="capitalize text-xl font-bold mb-3">
+              Doraemon và những hiệp sĩ không gian
+            </h3>
+            <div className="flex justify-between items-center text-sm opacity-50 mb-10">
+              <span>2017</span>
+              <span>7.4</span>
+            </div>
+            <button className="py-3 px-6 bg-primary rounded-lg text-white font-medium capitalize w-full">
+              Watch now
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="movies-layout page-container pb-20">
+        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
+          Top rated
+        </h2>
+        <div className="movie-list grid grid-cols-4 gap-10">
+          <div className="movie-card rounded-lg p-3 bg-slate-800 text-white">
+            <img
+              src="https://s3.cloud.cmctelecom.vn/tinhte1/2018/05/4308115_preview.medium.jpg"
+              alt=""
+              className="w-full h-[250px] rounded-lg object-cover mb-5"
+            />
+            <h3 className="capitalize text-xl font-bold mb-3">
+              Doraemon và những hiệp sĩ không gian
+            </h3>
+            <div className="flex justify-between items-center text-sm opacity-50 mb-10">
+              <span>2017</span>
+              <span>7.4</span>
+            </div>
+            <button className="py-3 px-6 bg-primary rounded-lg text-white font-medium capitalize w-full">
+              Watch now
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="movies-layout page-container pb-20">
+        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
+          Trending
+        </h2>
+        <div className="movie-list grid grid-cols-4 gap-10">
+          <div className="movie-card rounded-lg p-3 bg-slate-800 text-white">
+            <img
+              src="https://s3.cloud.cmctelecom.vn/tinhte1/2018/05/4308115_preview.medium.jpg"
+              alt=""
+              className="w-full h-[250px] rounded-lg object-cover mb-5"
+            />
+            <h3 className="capitalize text-xl font-bold mb-3">
+              Doraemon và những hiệp sĩ không gian
+            </h3>
+            <div className="flex justify-between items-center text-sm opacity-50 mb-10">
+              <span>2017</span>
+              <span>7.4</span>
+            </div>
+            <button className="py-3 px-6 bg-primary rounded-lg text-white font-medium capitalize w-full">
               Watch now
             </button>
           </div>
