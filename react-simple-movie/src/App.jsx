90c9d26fbdbf2ffe40635f1e1484cac047b669d6
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-11-02 17:23:48 +0700

    b212: Thiết lập Routes cho dự án

--- react-simple-movie/src/App.jsx
+++ react-simple-movie/src/App.jsx
@@ -2,35 +2,29 @@
 
 // Import Swiper styles
 import "swiper/css";
-import MovieList from "./components/movie/MovieList";
+import { Routes, Route } from "react-router-dom";
+import Main from "./components/layout/Main";
+import HomePage from "./pages/HomePage";
 import Banner from "./components/banner/Banner";
+import MoviePage from "./pages/MoviePage";
 
 function App() {
   return (
     <>
-      <header className="header flex gap-x-5 items-center justify-center text-white py-10 mb-5">
-        <span className="text-primary">Home</span>
-        <span>Movies</span>
-      </header>
-      <Banner></Banner>
-      <section className="movies-layout page-container pb-20">
-        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
-          Now playing
-        </h2>
-        <MovieList></MovieList>
-      </section>
-      <section className="movies-layout page-container pb-20">
-        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
-          Top rated
-        </h2>
-        <MovieList type="top_rated"></MovieList>
-      </section>
-      <section className="movies-layout page-container pb-20">
-        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
-          Trending
-        </h2>
-        <MovieList type="popular"></MovieList>
-      </section>
+      <Routes>
+        <Route element={<Main></Main>}>
+          <Route
+            path="/"
+            element={
+              <>
+                <Banner></Banner>
+                <HomePage></HomePage>
+              </>
+            }
+          ></Route>
+          <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
+        </Route>
+      </Routes>
     </>
   );
 }
---
+++ react-simple-movie/src/pages/MoviePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MoviePage = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, repudiandae
+      ut? Ut adipisci aspernatur in inventore nisi fugit consequatur quidem
+      molestias aut voluptatum. Dolore earum quibusdam eaque voluptate
+      voluptatem magni!
+    </div>
+  );
+};
+
+export default MoviePage;
