f10b6a056ffb119db3cc54ef67bf5ee5c2745f86
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2024-12-29 15:53:17 +0700

    b221: Tối ưu code phần 3

--- react-simple-movie/src/components/movie/MovieCard.jsx
+++ react-simple-movie/src/components/movie/MovieCard.jsx
@@ -4,6 +4,7 @@ import Button from "../buttons/Button";
 import { tmdbAPI } from "../../config";
 import PropTypes from "prop-types";
 import { withErrorBoundary } from "react-error-boundary";
+import LoadingSkeleton from "../loading/LoadingSkeleton";
 
 const MovieCard = ({ item }) => {
   const { title, release_date, vote_average, poster_path, id } = item;
@@ -55,3 +56,34 @@ function FallbackComponent() {
 export default withErrorBoundary(MovieCard, {
   FallbackComponent,
 });
+
+export const MovieCardSkeleton = () => {
+  return (
+    <div className="movie-card flex flex-col rounded-lg p-3 bg-slate-800 text-white h-full select-none">
+      <LoadingSkeleton
+        width="100%"
+        height="250px"
+        borderRadius="8px"
+        className="mb-5"
+      ></LoadingSkeleton>
+      <div className="flex flex-col flex-1">
+        <h3 className="capitalize text-xl font-bold mb-3">
+          <LoadingSkeleton width="100%" height="20px"></LoadingSkeleton>
+        </h3>
+        <div className="flex justify-between items-center text-sm opacity-50 mb-10">
+          <span>
+            <LoadingSkeleton width="50px" height="10px"></LoadingSkeleton>
+          </span>
+          <span>
+            <span>
+              <LoadingSkeleton width="30px" height="10px"></LoadingSkeleton>
+            </span>
+          </span>
+        </div>
+        <span>
+          <LoadingSkeleton width="100%" height="45px" borderRadius="6px"></LoadingSkeleton>
+        </span>
+      </div>
+    </div>
+  );
+};
--- react-simple-movie/src/components/movie/MovieList.jsx
+++ react-simple-movie/src/components/movie/MovieList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import MovieCard from "./MovieCard";
+import MovieCard, { MovieCardSkeleton } from "./MovieCard";
 import useSWR from "swr";
 import { fetcher, tmdbAPI } from "../../config";
 import PropTypes from "prop-types";
@@ -10,28 +10,39 @@ import { withErrorBoundary } from "react-error-boundary";
 
 const MovieList = ({ type = "now_playing" }) => {
   // const [movies, setMovies] = useState([]);
-  const { data, error, isLoading } = useSWR(
-    tmdbAPI.getMovieList(type),
-    fetcher
-  );
+  const { data, error } = useSWR(tmdbAPI.getMovieList(type), fetcher);
   // console.log("🚀 ~ MovieList ~ data:", data);
 
   // useEffect(() => {
   //   if (data && data.results) setMovies(data.results);
   // }, [data]);
+  const isLoading = !data && !error;
   const movies = data?.results || [];
 
   console.log("🚀 ~ MovieList ~ movies:", movies);
   return (
     <div className="movie-list">
-      <Swiper spaceBetween={40} grabCursor={"true"} slidesPerView={"auto"}>
-        {movies.length > 0 &&
-          movies.map((item) => (
-            <SwiperSlide key={item.id}>
-              <MovieCard item={item}></MovieCard>
-            </SwiperSlide>
-          ))}
-      </Swiper>
+      {isLoading && (
+        <>
+          <Swiper spaceBetween={40} grabCursor={"true"} slidesPerView={"auto"}>
+            {new Array(7).fill(0).map((_, index) => (
+              <SwiperSlide key={index}>
+                <MovieCardSkeleton></MovieCardSkeleton>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </>
+      )}
+      {!isLoading && (
+        <Swiper spaceBetween={40} grabCursor={"true"} slidesPerView={"auto"}>
+          {movies.length > 0 &&
+            movies.map((item) => (
+              <SwiperSlide key={item.id}>
+                <MovieCard item={item}></MovieCard>
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      )}
     </div>
   );
 };
