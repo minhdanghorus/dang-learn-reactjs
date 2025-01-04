e216d4cd6a374108320f28988943c5ecb760dc8f
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2025-01-04 20:55:55 +0700

    b230: Hiển thị realtime với snapshot

--- my-app/src/firebase/FirebaseApp.jsx
+++ my-app/src/firebase/FirebaseApp.jsx
@@ -1,6 +1,14 @@
-import { addDoc, collection, deleteDoc, doc, getDocs } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  onSnapshot,
+} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "./firebase-config";
+import { v4 } from "uuid";
 
 const FirebaseApp = () => {
   // colRef
@@ -8,24 +16,40 @@ const FirebaseApp = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [postId, setPostId] = useState("");
+  const [posts, setPosts] = useState([]);
   //   console.log("colRef", colRef);
   useEffect(() => {
     // 1. Get collection data (posts)
-    getDocs(colRef)
-      .then((snapshot) => {
-        // console.log("snapshot", snapshot);
-        let posts = [];
-        snapshot.docs.forEach((doc) => {
-          posts.push({
-            id: doc.id,
-            ...doc.data(),
-          });
+    // getDocs(colRef)
+    //   .then((snapshot) => {
+    //     // console.log("snapshot", snapshot);
+    //     let posts = [];
+    //     snapshot.docs.forEach((doc) => {
+    //       posts.push({
+    //         id: doc.id,
+    //         ...doc.data(),
+    //       });
+    //     });
+    //     console.log("posts", posts);
+    //     setPosts(posts);
+    //   })
+    //   .catch((error) => {
+    //     console.log("Error getting documents: ", error);
+    //   });
+
+    //2. Realtime listener
+    onSnapshot(colRef, (snapshot) => {
+      let posts = [];
+      snapshot.docs.forEach((doc) => {
+        posts.push({
+          id: doc.id,
+          ...doc.data(),
         });
-        console.log("posts", posts);
-      })
-      .catch((error) => {
-        console.log("Error getting documents: ", error);
       });
+      console.log("posts", posts);
+      setPosts(posts);
+    }
+    );
   }, []);
 
   const handleAddPost = (e) => {
@@ -55,45 +79,58 @@ const FirebaseApp = () => {
     console.log("Document successfully deleted!");
   };
   return (
-    <div className="w-full max-w-[500px] mx-auto bg-white shadow-lg p-5">
-      <form action="" onSubmit={handleAddPost} className="mb-10">
-        <input
-          type="text"
-          className="p-3 rounded border border-gray-200 w-full mb-5 outline-none focus:border-blue-500"
-          placeholder="Enter your title"
-          name="title"
-          onChange={(e) => setTitle(e.target.value)}
-        ></input>
-        <input
-          type="text"
-          className="p-3 rounded border border-gray-200 w-full mb-5 outline-none focus:border-blue-500"
-          placeholder="Enter your author"
-          name="author"
-          onChange={(e) => setAuthor(e.target.value)}
-        ></input>
-        <button
-          type="submit"
-          className="p-3 bg-blue-500 text-white text-sm font-medium w-full rounded-lg"
-        >
-          Add post
-        </button>
-      </form>
+    <div className="p-10">
+      <div className="w-full max-w-[500px] mx-auto bg-white shadow-lg p-5">
+        <form action="" onSubmit={handleAddPost} className="mb-10">
+          <input
+            type="text"
+            className="p-3 rounded border border-gray-200 w-full mb-5 outline-none focus:border-blue-500"
+            placeholder="Enter your title"
+            name="title"
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
+          <input
+            type="text"
+            className="p-3 rounded border border-gray-200 w-full mb-5 outline-none focus:border-blue-500"
+            placeholder="Enter your author"
+            name="author"
+            onChange={(e) => setAuthor(e.target.value)}
+          ></input>
+          <button
+            type="submit"
+            className="p-3 bg-blue-500 text-white text-sm font-medium w-full rounded-lg"
+          >
+            Add post
+          </button>
+        </form>
 
-      <form action="" onSubmit={handleRemovePost}>
-        <input
-          type="text"
-          className="p-3 rounded border border-gray-200 w-full mb-5 outline-none focus:border-blue-500"
-          placeholder="Enter your id"
-          name="PostId"
-          onChange={(e) => setPostId(e.target.value)}
-        ></input>
-        <button
-          type="submit"
-          className="p-3 bg-red-500 text-white text-sm font-medium w-full rounded-lg"
-        >
-          Delete post
-        </button>
-      </form>
+        <form action="" onSubmit={handleRemovePost}>
+          <input
+            type="text"
+            className="p-3 rounded border border-gray-200 w-full mb-5 outline-none focus:border-blue-500"
+            placeholder="Enter your id"
+            name="PostId"
+            onChange={(e) => setPostId(e.target.value)}
+          ></input>
+          <button
+            type="submit"
+            className="p-3 bg-red-500 text-white text-sm font-medium w-full rounded-lg"
+          >
+            Delete post
+          </button>
+        </form>
+      </div>
+
+      <div className="w-full max-w-[500px] mx-auto bg-white shadow-lg p-5">
+        {posts.length > 0 &&
+          posts.map((item) => {
+            return (
+              <div className="p-1" key={item.id}>
+                {item.title} - {item.author}
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
