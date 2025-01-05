fbc90dcbe14e4b47596efafd31043dd1a3bca050
minhdanghorus <nguyen.minh.dang@horusoftaceae.com>
2025-01-05 11:38:45 +0700

    b238: Chức năng đăng nhập

--- my-app/src/firebase/FirebaseAuth.jsx
+++ my-app/src/firebase/FirebaseAuth.jsx
@@ -28,7 +28,7 @@ const FirebaseAuth = () => {
 
     // Cleanup subscription on unmount
     return () => unsubscribe();
-  }, []); // Empty dependency array - only run once on mount
+  }, [userInfo]); // only run once on mount
   const handleInputChange = (e) => {
     setValues({
       ...values,
@@ -51,7 +51,7 @@ const FirebaseAuth = () => {
       await updateProfile(auth.currentUser, {
         displayName: "Jane Q. User",
       });
-    //   setUserInfo(user);
+      setUserInfo(credential);
 
       const userRef = collection(db, "users");
       await addDoc(userRef, {
@@ -65,6 +65,15 @@ const FirebaseAuth = () => {
   };
   const handleSignOut = () => {
     signOut(auth);
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    // credential
+    const cred = await signInWithEmailAndPassword(auth, values.email, values.password);
+    setUserInfo(cred);
+    console.log("cred: ", cred);
+    console.log("Login Successfully")
   };
   return (
     <div className="p-10">
@@ -100,6 +109,31 @@ const FirebaseAuth = () => {
             Sign out
           </button>
         </div>
+      </div>
+
+      <div className="w-full max-w-[500px] mx-auto bg-white shadow-lg p-5">
+        <form action="" className="mb-10" onSubmit={handleLogin}>
+          <input
+            type="text"
+            className="p-3 rounded border border-gray-200 w-full mb-5 outline-none focus:border-blue-500"
+            placeholder="Enter your email"
+            name="email"
+            onChange={handleInputChange}
+          ></input>
+          <input
+            type="password"
+            className="p-3 rounded border border-gray-200 w-full mb-5 outline-none focus:border-blue-500"
+            placeholder="Enter your password"
+            name="password"
+            onChange={handleInputChange}
+          ></input>
+          <button
+            type="submit"
+            className="p-3 bg-pink-500 text-white text-sm font-medium w-full rounded-lg"
+          >
+            Login
+          </button>
+        </form>
       </div>
     </div>
   );
